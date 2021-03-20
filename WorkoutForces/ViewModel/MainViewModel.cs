d6f05a22e4d4f66cfa4ca1136b43d4c296f67b91
truccm2001 <truccm2001@outlook.com>
2021-03-20 16:07:18 +0700

    yugyug

---
+++ WorkoutForces/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutForces.ViewModel
+{
+    class MainViewModel
+    {
+        RankViewModel RankView;
+
+        MainViewModel()
+        {
+            RankView = new RankViewModel();
+        }
+    }
+}
---
+++ WorkoutForces/ViewModel/RankViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutForces.Model;
+
+namespace WorkoutForces.ViewModel
+{
+    class RankViewModel
+    {
+        public ObservableCollection<User> listRank;
+        public RelayCommand<object> RankCommand;
+
+        public RankViewModel()
+        {
+            listRank = new ObservableCollection<User>();
+            RankCommand = new RelayCommand<object>(canExecuteRank, RankExecute);
+        }
+
+        public bool canExecuteRank(object x)
+        {
+            return true;
+        }
+        public void RankExecute(object p)
+        {
+            listRank = DataProvider.Ins.DB.Users.OrderByDescending(x => x.score).Take(10).ToList();               
+        }
+
+    }
+}
